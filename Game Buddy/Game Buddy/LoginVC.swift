b0c6f2052ebbb0137d8b59e2cc7bdae4edbbf3da
Carlos Costa <30331609+capcosta@users.noreply.github.com>
2017-07-21 15:45:30 -0700

    Added login/register controller.

---
+++ Game Buddy/Game Buddy/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Game Buddy
+//
+//  Created by Carlos on 2017-07-21.
+//  Copyright © 2017 Carlos da Costa. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+			print("LoginVC()")
+			view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+		}
+
+
+	override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+	
+  }
--- Game Buddy/Game Buddy/ViewController.swift
+++ Game Buddy/Game Buddy/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
 
 }
 
+extension UIColor {
+	convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+		self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+	}
+}
